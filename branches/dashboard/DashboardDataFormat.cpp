b70ef320ea39050f650f0d46152b3cd9a0fbf2ea
first.team1100 <first.team1100@ddad09ec-df38-11dd-a8eb-ddf07f3c1838>
2009-01-13 02:21:31 +0000

    Branch for dealing with dashboard communications.

---
+++ branches/dashboard/DashboardDataFormat.cpp
@@ -0,0 +1,55 @@
+
+#include "DashboardDataFormat.h"
+
+DashboardDataFormat::DashboardDataFormat(void) : m_ds (DriverStation::GetInstance())
+{
+	
+}
+
+DashboardDataFormat::~DashboardDataFormat()
+{
+	
+}
+
+/**
+ * Pack data using the correct types and in the correct order to match the
+ * default "Dashboard Datatype" in the LabVIEW Dashboard project.
+ */
+void DashboardDataFormat::PackAndSend(void)
+{
+	Dashboard &dashboardPacker = m_ds->GetDashboardPacker();
+	UINT32 module;
+	UINT32 channel;
+
+	// Pack the analog modules
+	for (module = 0; module < kAnalogModules; module++)
+	{
+		dashboardPacker.AddCluster();
+		for (channel = 0; channel < kAnalogChannels; channel++)
+		{
+			dashboardPacker.AddFloat(m_analogChannels[module][channel]);
+		}
+		dashboardPacker.FinalizeCluster();
+	}
+	// Pack the digital modules
+	for (module = 0; module < kDigitalModules; module++)
+	{
+		dashboardPacker.AddCluster();
+		dashboardPacker.AddU8(m_relayFwd[module]);
+		dashboardPacker.AddU8(m_relayRev[module]);
+		dashboardPacker.AddU16(m_dioChannels[module]);
+		dashboardPacker.AddU16(m_dioChannelsOutputEnable[module]);
+		dashboardPacker.AddCluster();
+		for(channel = 0; channel < kPwmChannels; channel++)
+		{
+			dashboardPacker.AddU8(m_pwmChannels[module][channel]);
+		}
+		dashboardPacker.FinalizeCluster();
+		dashboardPacker.FinalizeCluster();
+	}
+	// Pack the solenoid module
+	dashboardPacker.AddU8(m_solenoidChannels);
+
+	// Flush the data to the driver station.
+	dashboardPacker.Finalize();
+}
