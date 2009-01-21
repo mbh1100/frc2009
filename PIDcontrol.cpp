09282fd949bc951c690bbea23401078cebe5ea31
first.team1100 <first.team1100@ddad09ec-df38-11dd-a8eb-ddf07f3c1838>
2009-01-21 00:46:57 +0000

    PID loop success! Working on scanTarget --Faith

--- PIDcontrol.cpp
+++ PIDcontrol.cpp
@@ -11,25 +11,47 @@ PIDcontrol::PIDcontrol(float p, float i, float d, float allowedError)
 	m_increment = 0.01;
 	m_desiredValue = 0;
 	m_cycleCount = 0;
+	m_previousValue = 0;
 }
-void PIDcontrol::setDesired(float maxOutput, float minOutput, float increment, float desiredValue)
+void PIDcontrol::setDesired(float desiredValue)
+{
+	m_desiredValue = desiredValue;
+}
+void PIDcontrol::setDesired(float p, float i, float d)
+{
+	m_p = p;
+	m_i = i;
+	m_d = d;
+}
+void PIDcontrol::setDesired(float maxOutput, float minOutput, float increment, float desiredValue, float relation)
 {
 	m_maxOutput = maxOutput;
 	m_minOutput = minOutput;
 	m_increment = increment;
 	m_desiredValue = desiredValue;
+	m_relateInOut = relation;
+}
+void PIDcontrol::setDesired(float maxOutput, float minOutput, float increment, float desiredValue, float relation, float p, float i, float d, float allowedError)
+{
+	m_maxOutput = maxOutput;
+	m_minOutput = minOutput;
+	m_increment = increment;
+	m_desiredValue = desiredValue;
+	m_p = p;
+	m_i = i;
+	m_d = d;
+	m_allowedError = allowedError;
+	m_relateInOut = relation;
 }
 void PIDcontrol::resetLoop()
 {
+	printf("...Reset \n");
+	m_previousValue = 0;
 	m_errorSum = 0;
 	m_cycleCount = 0;
 }
 float PIDcontrol::calcPID(float current)
 {
-	if(fabs(m_desiredValue - current) > fabs(m_previousValue))
-	{
-		resetLoop();
-	}
 	float pVal = 0.0, iVal = 0.0, dVal = 0.0;
 	
 	if(m_cycleCount == 0)
@@ -39,6 +61,7 @@ float PIDcontrol::calcPID(float current)
 	
 	/* Calculate P component */
 	float error = m_desiredValue - current;
+	error *= m_relateInOut;
 	pVal = m_p * error;
 	
 	/* Calculate I component */
@@ -48,9 +71,9 @@ float PIDcontrol::calcPID(float current)
 		{
 			m_errorSum = 0;
 		}
-		if((error/1000) < m_increment)
+		if((error) < m_increment)
 		{
-			m_errorSum += (error/1000);
+			m_errorSum += error;
 		}
 		else
 		{
@@ -101,6 +124,7 @@ bool PIDcontrol::isDone(float current)
 {
 	if (fabs(current) <= m_desiredValue + m_allowedError)
 	{
+		printf("%ud Resetting in isDone",(unsigned int)this);
 		resetLoop();
 		return true;
 	}
--- TrackingTurret.cpp
+++ TrackingTurret.cpp
@@ -10,28 +10,49 @@ TrackingTurret::TrackingTurret()
 	m_servo8 = new Servo(8);
 	m_motor7 = new Jaguar(7);
 	
-	m_p = 0.0001;
-	m_i = 0.0001;
-	m_d = 0.002;
+	//All variables
+	m_pX = 0.1;
+	m_iX = 0;//0.0001;
+	m_dX = 0;//0.0002;
+	m_pY = 0.01;
+	m_iY = 0.0001;
+	m_dY = 0.002;
 	m_allowedErrorX = 100;
-	m_allowedErrorY = 100;
-	m_maxOutX = 0.05;
-	m_minOutX = -0.05;
+	m_allowedErrorY = 50;
+	m_maxOutX = 0.1;
+	m_minOutX = -0.1;
+	m_relationX = 0.0005;
 	m_incrementX = 0.001;
 	m_desiredX = 0.0;
 	m_maxOutY = 0.01;
 	m_minOutY = 0.0;
+	m_relationY = 0.0005;
 	m_incrementY = .001;
 	m_desiredY = 0.0;	
 	
-	calcSpeedX = new PIDcontrol(m_p, m_i, m_d, m_allowedErrorX);
-	calcSpeedY = new PIDcontrol(m_p, m_i, m_d, m_allowedErrorY);
-	calcSpeedX->setDesired(m_maxOutX, m_minOutX, m_incrementX, m_desiredX);
-	calcSpeedY->setDesired(m_maxOutY, m_minOutY, m_incrementX, m_desiredX);
+	m_calcSpeedX = new PIDcontrol(m_pX, m_iX, m_dX, m_allowedErrorX);
+	m_calcSpeedY = new PIDcontrol(m_pY, m_iY, m_dY, m_allowedErrorY);
+	m_calcSpeedX->setDesired(m_maxOutX, m_minOutX, m_incrementX, m_desiredX, m_relationX);
+	m_calcSpeedY->setDesired(m_maxOutY, m_minOutY, m_incrementX, m_desiredX, m_relationY);
+	
+	m_scanLoop = 0;
+	m_inverse = 0;
+	m_scanP = .1;
+	m_scanI = 0;
+	m_scanD = 0;
+	m_maxOutScan = .2;
+	m_minOutScan = -.2;
+	m_potLeft = 100;
+	m_potRight = 500;
+	m_calcScan = new PIDcontrol(m_scanP, m_scanI, m_scanD,10.0);
+	m_calcScan->setDesired(m_maxOutScan, m_minOutScan, 0.0, m_potLeft, m_relationX);
 }
 bool TrackingTurret::aimTurret(float distanceX, float distanceY)
 {	
-	if(calcSpeedX->isDone(distanceX) && calcSpeedY->isDone(distanceY))
+	bool xDone,yDone;
+	xDone = m_calcSpeedX->isDone(distanceX);
+	yDone = m_calcSpeedY->isDone(distanceY);
+	if(xDone && yDone)
 	{
 		m_servo8->Set(0.0);
 		m_motor7->Set(0.0);
@@ -39,17 +60,17 @@ bool TrackingTurret::aimTurret(float distanceX, float distanceY)
 	}
 	else
 	{
-		if(!calcSpeedX->isDone(distanceX))
+		if(!xDone)
 		{
-			m_valueX = calcSpeedX->calcPID(distanceX);
+			m_valueX = m_calcSpeedX->calcPID(distanceX);
 		}
 		else
 		{
 			m_valueX = 0.0;
 		}
-		if(!calcSpeedY->isDone(distanceY))
+		if(!yDone)
 		{
-			m_valueY = calcSpeedY->calcPID(distanceY);
+			m_valueY = m_calcSpeedY->calcPID(distanceY);
 			m_currentY = m_servo8->Get();
 			m_currentY += m_valueY;
 			if (m_currentY > m_maxServo);
@@ -65,7 +86,7 @@ bool TrackingTurret::aimTurret(float distanceX, float distanceY)
 		printf("Output to motor: %f\n", m_valueX);
 		printf("Output to servo: %f\n", m_currentY);
 		
-		if (m_valueX > 0)
+		if(m_valueX > 0)
 		{
 			m_valueX += .05;
 		}
@@ -79,6 +100,31 @@ bool TrackingTurret::aimTurret(float distanceX, float distanceY)
 		return false;
 	}
 }
-void TrackingTurret::scanTarget(float currentX, float currentY)
+void TrackingTurret::scanTarget(float currentX)
 {
+	if(m_scanLoop <= 0)
+	{
+		if(currentX > 0)
+		{
+			m_inverse = 1;
+			m_calcScan->setDesired(m_potMax);
+		}
+		else
+		{
+			m_inverse = 0;
+			m_calcScan->setDesired(m_potMin);
+		}
+	}
+	
+	
+	m_scanLoop++;	
+}
+void TrackingTurret::resetScan()
+{
+	m_scanLoop = 0;
+}
+void TrackingTurret::stopTurret()
+{
+	m_servo8->Set(0.0);
+	m_motor7->Set(0.0);
 }
