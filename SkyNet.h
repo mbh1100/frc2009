b2ad966680b5d267c4206879f869ccaffd2ffbf6
Hunter0000 <Hunter0000@ddad09ec-df38-11dd-a8eb-ddf07f3c1838>
2009-01-22 22:18:35 +0000

    Cleaned up Skynet.h and Skynet.cpp to prepare for the merge --Faith

--- SkyNet.h
+++ SkyNet.h
@@ -26,23 +26,12 @@ public:
 	TrackingCamera *m_trackingCamera;
 	TrackingTurret *m_trackingTurret;
 	
-	Joystick *m_rightStick,*m_leftStick;
-	
-	RobotDrive *m_RobotDrive;
-	Jaguar *m_motor1,*m_motor2,*m_motor3,*m_motor4;
-	Servo *m_servo1;
-	
-	AnalogModule *m_analogModules[2];
-	
 	DriverStation *m_ds;
 	UINT32 m_priorPacketNumber;
 	UINT8 m_dsPacketsPerSecond;
 	
 	UINT32 m_autoCount,m_teleCount,m_printsPerLoop;
 		
-	//Dashboard Stuff
-	PCVideoServer *m_camToDash;
-	
 	//Camera Locating
 	bool m_inView;
 	float m_positionX,m_positionY;
